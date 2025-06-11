86f73af90a4a410186793803594eb24c87265089
phucnguyen <phuc.nguyen@siliconstack.com.au>
2025-06-11 07:59:31 +0700

    Update Unit test for Customer Entity

--- src/Tests/SalesManagement.Domain.Test/Customers/CustomerTest.cs
+++ src/Tests/SalesManagement.Domain.Test/Customers/CustomerTest.cs
@@ -1,182 +1,143 @@
-// using SalesManagement.Domain.Customers;
-// using SalesManagement.Domain.Customers.ValueObjects;
-//
-// namespace SalesManagement.Domain.Test.Customers;
-//
-// public class CustomerTest
-// {
-//     [Fact]
-//     public void IsValid_ValidCustomer_Successfully()
-//     {
-//         var validCustomer = new Customer
-//         {
-//             FirstName = "John",
-//             LastName = "Doe",
-//             Email = new EmailAddress
-//             {
-//                 Value = "johndoe@gmail.com"
-//             },
-//             DateOfBirth = new DateTime(1980, 01, 01),
-//             DeliveryAddress = new DeliveryAddress
-//             {
-//                 Address = "123 Main Street",
-//                 City = "London",
-//                 Country = "England",
-//             }
-//         };
-//         
-//         var actualResult = validCustomer.IsValid();
-//         
-//         Assert.True(actualResult);
-//     }
-//     
-//     [Fact]
-//     public void IsValid_EmptyFirstName_ReturnFalse()
-//     {
-//         var validCustomer = new Customer
-//         {
-//             FirstName = string.Empty,
-//             LastName = "Doe",
-//             Email = new EmailAddress
-//             {
-//                 Value = "johndoe@gmail.com"
-//             },
-//             DateOfBirth = new DateTime(1980, 01, 01),
-//             DeliveryAddress = new DeliveryAddress
-//             {
-//                 Address = "123 Main Street",
-//                 City = "London",
-//                 Country = "England",
-//             }
-//         };
-//         
-//         var actualResult = validCustomer.IsValid();
-//         
-//         Assert.False(actualResult);
-//     }
-//     
-//     [Fact]
-//     public void IsValid_EmptyLastName_ReturnFalse()
-//     {
-//         var validCustomer = new Customer
-//         {
-//             FirstName = "John",
-//             LastName = string.Empty,
-//             Email = new EmailAddress
-//             {
-//                 Value = "johndoe@gmail.com"
-//             },
-//             DateOfBirth = new DateTime(1980, 01, 01),
-//             DeliveryAddress = new DeliveryAddress
-//             {
-//                 Address = "123 Main Street",
-//                 City = "London",
-//                 Country = "England",
-//             }
-//         };
-//         
-//         var actualResult = validCustomer.IsValid();
-//         
-//         Assert.False(actualResult);
-//     }
-//     
-//     [Fact]
-//     public void IsValid_InvalidEmail_ReturnFalse()
-//     {
-//         var validCustomer = new Customer
-//         {
-//             FirstName = "John",
-//             LastName = string.Empty,
-//             Email = new EmailAddress
-//             {
-//                 Value = "john"
-//             },
-//             DateOfBirth = new DateTime(1980, 01, 01),
-//             DeliveryAddress = new DeliveryAddress
-//             {
-//                 Address = "123 Main Street",
-//                 City = "London",
-//                 Country = "England",
-//             }
-//         };
-//         
-//         var actualResult = validCustomer.IsValid();
-//         
-//         Assert.False(actualResult);
-//     }
-//     
-//     [Fact]
-//     public void IsValid_InvalidAddress_ReturnFalse()
-//     {
-//         var validCustomer = new Customer
-//         {
-//             FirstName = "John",
-//             LastName = string.Empty,
-//             Email = new EmailAddress
-//             {
-//                 Value = "john@gmail.com"
-//             },
-//             DateOfBirth = new DateTime(1980, 01, 01),
-//             DeliveryAddress = new DeliveryAddress
-//             {
-//                 Address = string.Empty,
-//                 City = "London",
-//                 Country = "England",
-//             }
-//         };
-//         
-//         var actualResult = validCustomer.IsValid();
-//         
-//         Assert.False(actualResult);
-//     }
-//     
-//     [Fact]
-//     public void IsValid_InvalidCity_ReturnFalse()
-//     {
-//         var validCustomer = new Customer
-//         {
-//             FirstName = "John",
-//             LastName = string.Empty,
-//             Email = new EmailAddress
-//             {
-//                 Value = "john@gmail.com"
-//             },
-//             DateOfBirth = new DateTime(1980, 01, 01),
-//             DeliveryAddress = new DeliveryAddress
-//             {
-//                 Address = "123 Main Street",
-//                 City = string.Empty,
-//                 Country = "England",
-//             }
-//         };
-//         
-//         var actualResult = validCustomer.IsValid();
-//         
-//         Assert.False(actualResult);
-//     }
-//     
-//     [Fact]
-//     public void IsValid_InvalidCountry_ReturnFalse()
-//     {
-//         var validCustomer = new Customer
-//         {
-//             FirstName = "John",
-//             LastName = string.Empty,
-//             Email = new EmailAddress
-//             {
-//                 Value = "john@gmail.com"
-//             },
-//             DateOfBirth = new DateTime(1980, 01, 01),
-//             DeliveryAddress = new DeliveryAddress
-//             {
-//                 Address = "123 Main Street",
-//                 City = "London",
-//                 Country = string.Empty,
-//             }
-//         };
-//         
-//         var actualResult = validCustomer.IsValid();
-//         
-//         Assert.False(actualResult);
-//     }
-// }
+using SalesManagement.Domain.Customers;
+using SalesManagement.Domain.Customers.ValueObjects;
+
+namespace SalesManagement.Domain.Test.Customers;
+
+public class CustomerTest
+{
+    [Fact]
+    public void IsValid_ValidCustomer_Successfully()
+    {
+        var validCustomer = new CustomerCreationModel
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@gmail.com",
+            DateOfBirth = new DateTime(1980, 01, 01),
+            Address = "123 Main Street",
+            City = "London",
+            Country = "England"
+        };
+        
+        var actualResult = Customer.Create(validCustomer);
+        
+        Assert.NotNull(actualResult);
+        Assert.True(actualResult.IsSuccess);
+        Assert.NotNull(actualResult.Entity);
+    }
+    
+    [Fact]
+    public void IsValid_EmptyFirstName_ReturnFalse()
+    {
+        var invalidCustomer = new CustomerCreationModel
+        {
+            LastName = "Doe",
+            Email = "john.doe@gmail.com",
+            DateOfBirth = new DateTime(1980, 01, 01),
+            Address = "123 Main Street",
+            City = "London",
+            Country = "England"
+        };
+        
+        var actualResult = Customer.Create(invalidCustomer);
+        
+        Assert.False(actualResult.IsSuccess);
+        Assert.Null(actualResult.Entity);
+    }
+    
+    [Fact]
+    public void IsValid_EmptyLastName_ReturnFalse()
+    {
+        var invalidCustomer = new CustomerCreationModel
+        {
+            FirstName = "John",
+            Email = "john.doe@gmail.com",
+            DateOfBirth = new DateTime(1980, 01, 01),
+            Address = "123 Main Street",
+            City = "London",
+            Country = "England"
+        };
+        
+        var actualResult = Customer.Create(invalidCustomer);
+        
+        Assert.False(actualResult.IsSuccess);
+        Assert.Null(actualResult.Entity);
+    }
+    
+    [Fact]
+    public void IsValid_InvalidEmail_ReturnFalse()
+    {
+        var invalidCustomer = new CustomerCreationModel
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe",
+            DateOfBirth = new DateTime(1980, 01, 01),
+            Address = "123 Main Street",
+            City = "London",
+            Country = "England"
+        };
+        
+        var actualResult = Customer.Create(invalidCustomer);
+        
+        Assert.False(actualResult.IsSuccess);
+        Assert.Null(actualResult.Entity);
+    }
+    
+    [Fact]
+    public void IsValid_InvalidAddress_ReturnFalse()
+    {
+        var invalidCustomer = new CustomerCreationModel
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@gmail.com",
+            DateOfBirth = new DateTime(1980, 01, 01),
+            City = "London",
+            Country = "England"
+        };
+        
+        var actualResult = Customer.Create(invalidCustomer);
+        
+        Assert.False(actualResult.IsSuccess);
+        Assert.Null(actualResult.Entity);
+    }
+    
+    [Fact]
+    public void IsValid_InvalidCity_ReturnFalse()
+    {
+        var invalidCustomer = new CustomerCreationModel
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@gmail.com",
+            DateOfBirth = new DateTime(1980, 01, 01),
+            Address = "123 Main Street",
+            Country = "England"
+        };
+        
+        var actualResult = Customer.Create(invalidCustomer);
+        
+        Assert.False(actualResult.IsSuccess);
+        Assert.Null(actualResult.Entity);
+    }
+    
+    [Fact]
+    public void IsValid_InvalidCountry_ReturnFalse()
+    {
+        var invalidCustomer = new CustomerCreationModel
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@gmail.com",
+            DateOfBirth = new DateTime(1980, 01, 01),
+            Address = "123 Main Street",
+            City = "London"
+        };
+        
+        var actualResult = Customer.Create(invalidCustomer);
+        
+        Assert.False(actualResult.IsSuccess);
+        Assert.Null(actualResult.Entity);
+    }
+}
